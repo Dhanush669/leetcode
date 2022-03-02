a7e838fac4509024706224396fe12eea9a064cf9
R. Dhanush <68895339+Dhanush669@users.noreply.github.com>
2022-03-02 11:02:01 +0530

    Time: 51 ms (42.13%), Space: 14 MB (73.22%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,23 +1,27 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        #pos=[]
-        pre=0
-        f=0
-        #print(t.index('t'),t.index('w'),t.index('n'))
-        for i in s:
-            if i not in t:
-                return False
-            if pre==0:
-                pre=t.index(i)
-            else:
-                pre=t.index(i)+f
-            # if len(pos)==0:
-            #     pos.append(t.index(i))
-            # else:
-            #     pos.append(t.index(i)+f)
-            f+= len(t[:t.index(i)])  +1
-            if f<pre:
-                return False
-            t=t[t.index(i)+1:]
-        return True    
+        # pre=0
+        # f=0
+        # for i in s:
+        #     if i not in t:
+        #         return False
+        #     if pre==0:
+        #         pre=t.index(i)
+        #     else:
+        #         pre=t.index(i)+f
+        #     f+= len(t[:t.index(i)])  +1
+        #     if f<pre:
+        #         return False
+        #     t=t[t.index(i)+1:]
+        # return True    
+        if len(s) > len(t):return False
+        if len(s) == 0:return True
+        subsequence=0
+        for i in range(0,len(t)):
+            if subsequence <= len(s) -1:
+                print(s[subsequence])
+                if s[subsequence]==t[i]:
+
+                    subsequence+=1
+        return True if subsequence >= len(s) else False
         
