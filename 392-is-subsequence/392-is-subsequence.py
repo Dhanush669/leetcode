5109a3b19e1542f47daa9fe381d861e7155b901b
R. Dhanush <68895339+Dhanush669@users.noreply.github.com>
2022-03-02 11:00:26 +0530

    Time: 42 ms (63.19%), Space: 14 MB (55.23%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,23 +1,23 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        pos=[]
+        #pos=[]
+        pre=0
         f=0
         #print(t.index('t'),t.index('w'),t.index('n'))
         for i in s:
             if i not in t:
                 return False
-            
-            if len(pos)==0:
-                pos.append(t.index(i))
+            if pre==0:
+                pre=t.index(i)
             else:
-                pos.append(t.index(i)+f)
+                pre=t.index(i)+f
+            # if len(pos)==0:
+            #     pos.append(t.index(i))
+            # else:
+            #     pos.append(t.index(i)+f)
             f+= len(t[:t.index(i)])  +1
-            if f<pos[-1]:
+            if f<pre:
                 return False
             t=t[t.index(i)+1:]
-        #print(pos)    
-        # for i in range(0,len(pos)-1)    :
-        #     if pos[i]>pos[i+1]:
-        #         return False
         return True    
         
