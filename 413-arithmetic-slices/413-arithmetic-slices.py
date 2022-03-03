4b06687577b578685c7a6bba123367200f109d75
R. Dhanush <68895339+Dhanush669@users.noreply.github.com>
2022-03-04 01:33:14 +0530

    Time: 45 ms (70.74%), Space: 14.2 MB (51.84%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -0,0 +1,64 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        # lis=[]
+        siz=len(nums)
+        res=0
+        if len(nums)<3:
+            return 0
+        a1=nums[0]
+        inc=0
+        po=2
+        d=nums[1]-a1
+        for i in range(1,len(nums)-1):
+            
+            if nums[i]-nums[i-1] == nums[i+1] - nums[i]:
+                inc+=1
+                res+=inc
+                
+            else:
+                inc=0
+                
+            
+#             n_1=po
+            
+#             po+=1
+#             an=a1+(n_1*d)
+#             print(n_1,"m-1",an,nums[i])
+#             if an==nums[i]:
+#                 res+=inc
+#                 inc+=1
+#             else:
+#                 s=len(nums[i:])
+#                 print(s)
+#                 if s<3:
+#                     #print(len(nums[i:]),nums[i:])
+#                     break
+                    
+#                 a1=nums[i]
+#                 d=nums[i+1]-a1     
+#                 inc=0
+#                 po=1
+#                 print(a1,d,po)
+                
+        # for i in range(0,len(nums)-2)    :
+        #     for j in range(len(nums),2,-1):
+        #         if len(nums[i:j])>=3:
+        #             #lis.append(nums[i:j])
+        #             t=nums[i:j]
+        #             d=t[1]-t[0]
+        #             a1=t[0]
+        #             n_1=len(t)-1
+        #             an=a1+(n_1*d)
+        #             if an==t[-1]:
+        #                 res+=1
+                    
+        # res=0
+        # for i in lis:
+        #     d=i[1]-i[0]
+        #     a1=i[0]
+        #     n_1=len(i)-1
+        #     an=a1+(n_1*d)
+        #     if an==i[-1]:
+        #         res+=1       
+        return res
+        
