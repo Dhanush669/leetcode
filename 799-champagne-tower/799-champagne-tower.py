3940b77e63d7c105d9819b0426ad2921d1b272d2
R. Dhanush <68895339+Dhanush669@users.noreply.github.com>
2022-03-05 12:59:39 +0530

    Time: 325 ms (18.14%), Space: 14.4 MB (29.41%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.py
@@ -0,0 +1,27 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        if poured<=1 and query_row!=0 and query_glass!=0:
+            return 0.00000
+        if poured<=1 and query_row==0 and query_glass==0:
+            if poured==1:
+                return 1.00000
+            return 0.00000
+        lis=[[0]*i for i in range(1,102)]
+        lis[0][0]=poured
+        for i in range(query_row+1):
+            for j in range(i+1):
+                q=(lis[i][j]-1)/2
+                if q>0:
+                    t=lis[i+1][j]
+                    t1=lis[i+1][j+1]
+                    lis[i+1][j]=t+q
+                    lis[i+1][j+1]=t1+q
+                    
+        print(lis)    
+        if lis[query_row][query_glass]>1:
+            return 1.00000
+        return lis[query_row][query_glass]
+        
+
+
+        
