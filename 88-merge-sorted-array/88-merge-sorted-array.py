580235bbb3b557f456e2e4b27f81e8138ad0760c
R. Dhanush <68895339+Dhanush669@users.noreply.github.com>
2022-03-09 01:40:42 +0530

    Time: 40 ms (82.95%), Space: 13.8 MB (92.42%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,29 +3,47 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i=0
-        j=0
-        k=0
-        lis=nums1[:m]
-        while i<m and j<n:
-            if lis[i]<=nums2[j]:
-                nums1[k]=lis[i]
-                i+=1
-                k+=1
+        # i=0
+        # j=0
+        # k=0
+        # lis=nums1[:m]
+        # while i<m and j<n:
+        #     if lis[i]<=nums2[j]:
+        #         nums1[k]=lis[i]
+        #         i+=1
+        #         k+=1
+        #     else:
+        #         nums1[k]=nums2[j]
+        #         j+=1
+        #         k+=1
+        # print(i,j,m,n,nums1)        
+        # if j<n:
+        #     x=0
+        #     for i1 in range(j,n):
+        #         nums1[k]=nums2[i1]
+        #         k+=1
+        # if i<m:
+        #     x=0
+        #     for i1 in range(i,m):
+        #         nums1[k]=lis[i1]
+        #         k+=1        
+        i=m-1
+        j=n-1
+        k=i+j+1
+        while i>=0 and j>=0:
+            if nums1[i]>nums2[j]:
+                nums1[k]=nums1[i]
+                i-=1
+                k-=1
             else:
                 nums1[k]=nums2[j]
-                j+=1
-                k+=1
-        print(i,j,m,n,nums1)        
-        if j<n:
-            x=0
-            for i1 in range(j,n):
-                nums1[k]=nums2[i1]
-                k+=1
-        if i<m:
-            x=0
-            for i1 in range(i,m):
-                nums1[k]=lis[i1]
-                k+=1        
-                
+                j-=1
+                k-=1
+        print(nums1)        
+        if j>=0:
+            for x in range(j,-1,-1):
+                nums1[x]=nums2[x]
+        #if i>0:
+            
+          
         
