3ec88523bd6d206e1a6f99e2400239d020713477
R. Dhanush <68895339+Dhanush669@users.noreply.github.com>
2022-04-26 11:26:36 +0530

    Time: 942 ms (5.05%), Space: 26 MB (72.98%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -2,12 +2,12 @@ from collections import defaultdict
 class Solution:
     def defa(self):
         return 0
+    
     def containsDuplicate(self, nums: List[int]) -> bool:
         dic=defaultdict(self.defa)
         for i in nums:
             dic[i]+=1
-        for i in dic:
-            if dic[i]>1:
+        for val in dic:
+            if dic[val]>1:
                 return True
         return False    
-        
